c2feddeccfbfcf8b4a1fad495331c09d2d7b6533
ubasjunril11 <ubasjunril11@gmail.com>
2024-07-12 16:42:49 +0800

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>CARL JAKE TUMAMPO</h1>
+      <h2>JERALYN PUERTO</h2>
+    </div>
+  );
+}
+
+export default App;
